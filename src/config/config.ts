2aecc373e36d19d4d31212b933998f193ae67cec
mtthw <matbalinton@gmail.com>
2025-01-23 10:40:37 +0800

    add authentication

---
+++ src/config/config.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAPuUHFKVrzMHA_CWxBITCqci6-RK5Beg",
+  authDomain: "pharma-kiosk-ad218.firebaseapp.com",
+  projectId: "pharma-kiosk-ad218",
+  storageBucket: "pharma-kiosk-ad218.appspot.com",
+  messagingSenderId: "270716874976",
+  appId: "1:270716874976:web:cd7c864190e61919912085",
+  measurementId: "G-4WMGN33EMH"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
---
+++ src/pages/RegistrationPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Register from '../components/auth/Register'
+
+const RegistrationPage : React.FC = () => {
+  return (
+    <>
+        <Register />
+    </>
+  )
+}
+
+export default RegistrationPage
