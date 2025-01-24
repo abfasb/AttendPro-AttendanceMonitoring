5909ab7a17c446125fc1eca6e9ceba992660fea4
mtthw <matbalinton@gmail.com>
2025-01-24 13:30:27 +0800

    add two pages for stud and intructor

---
+++ src/components/ui/Settings.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Settings : React.FC = () => {
+  return (
+    <div>
+        <h1>hello this is settings</h1>
+    </div>
+  );
+};
+
+export default Settings;
